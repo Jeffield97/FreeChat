62fa098917ee6ca77b1e78f268dac24023b991e3
Jeffield97 <jchariguaman@gmail.com>
2023-04-26 13:26:40 -0500

    First version

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.websockets import WebSocketDisconnect
+
+app = FastAPI()
+websockets= set()
+@app.get('/')
+def hello():
+    return {'Hello world'}
+
+# WebScket endpoint
+@app.websocket ('/ws')
+async def websocket_endpoint(websocket:WebSocket):
+    await websocket.accept()
+    websockets.add(websocket)
+    for ws in websockets:
+        await ws.send_text(f'New user {websocket.client.host} connected')
+    print(f'{websocket.client.host} connected')
+    while True:
+        try:
+            data = await websocket.receive_text()
+            for ws in websockets:
+                if ws!=websocket :
+                    await ws.send_text(data)
+        except WebSocketDisconnect:
+            websockets.remove(websocket)
